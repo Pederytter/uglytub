7246e19b6196bb65ca17bb43aa454f33ad564b71
manulul <eaaemnie1@students.eaaa.dk>
2017-01-31 10:10:20 +0100

    menukort

--- index.php
+++ index.php
@@ -1,19 +1,72 @@
+
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>VleminckxDeSausmeester</title>
 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<header>
-		<<nav>
+		<nav>
 			
 		</nav>
 	</header>
-	<main>
+    
+        <h1>Menu</h1>
+        <section class="menuleft">
+        <h3>Regular Sauces</h3>
+            <ol>
+                <li>Fries Sauce</li>
+                <li>Mayonnaise "The Classic"</li>
+                <li>Tomato Ketchup</li>
+                <li>Curry Ketchup</li>
+                <li>Satay Sauce</li>
+                <li>Sambal Sauce</li>
+                <li>Joppie Sauce</li>
+                <li>Piccalilly</li>
+                <li>Apple Sauce</li>
+                <li>Special mix Tomato Ketchup **<br>
+                    <i>with mayonnaise + onions</i>
+                <li>Special mix Curry ketchup **<br>
+                    <i>with mayonnaise + onions</i></li>
+                <li>Oorlog Mix **<br>
+                    <i>with mayonnaise
+                + satay sauce + onions</i>
+                <br>
+                <b>**DOUBLE PORTION OF SAUCE</b></li>
+            </ol>
+        </section>
+        <section class="menuright">
+        <h3>Belgian Sauces</h3>
+            <ol>
+                <li>Belgian Mayonnaise</li>
+                <li>Lemon Mayonnaise</li>
+                <li>Samurai Sauce</li>
+                <li>American Sauce</li>
+                <li>Tartar Sauce</li>
+                <li>Andalouse Sauce</li>
+                <li>Hannibal Sauce</li>
+                <li>Hawaii Sauce</li>
+                <li>Cocktail Sauce</li>
+                <li>Pickles Sauce</li>
+                <li>Green Pepper Sauce</li>
+                <li>Garlic Sauce</li>
+                <li>Mustard Sauce</li>
+                <li>Spicy Ketchup Sauce</li>
+                <li>Barbecue Sauce</li>
+                <li>Yellow Curry Sauce</li>
+            </ol>
+            
+            <h3>Regular Sauces</h3>
+            <p>Per sauce per portion: <br> <b>&euro; 0.70</b></p>
+            
+            <p>Double portion or 2 sauces: <br> <b>&euro; 1.00</b></p>
+        </section>
+    
+        <article></article>
 		
-	</main>
 	<footer>
 		
 	</footer>
