d33654060b6d57f855464c1e71dc14d446f75d75
Pederytter <pederytter@gmail.com>
2017-01-31 11:05:50 +0100

    Opdatering afheader

--- index.php
+++ index.php
@@ -1,31 +1,40 @@
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="bootstrap/scrolling-nav.css">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-	<header>
-		<nav class="col-md-12">
-			<ul>
-			<a href="#homepage"><img id="logo" src="images/logo.png" alt="Vleminck Logo"></a>
-				<li><a class="page-scroll" href="#homepage">Homepage</a></li>
-				<li><a class="page-scroll" href="#menu">Menu</a></li>
-				<li><a class="page-scroll" href="#about">About US</a></li>
-				<li><a class="page-scroll" href="#location">Location</a></li>
-			</ul>
-		</nav>
-	</header>
-	<main>
-		<section id="homepage" class="area"></section>
-		<section id="menu"></section>
-		<section id="about"></section>
-		<section id="location"></section>
-	</main>
-	<footer>
-		
-	</footer>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<header>
+			<nav class="col-md-12">
+				<ul>
+					<a href="#homepage"><img id="logo" src="images/logo.png" alt="Vleminck Logo"></a>
+					<li><a class="page-scroll" href="#homepage">Homepage</a></li>
+					<li><a class="page-scroll" href="#menu">Menu</a></li>
+					<li><a class="page-scroll" href="#about">About US</a></li>
+					<li><a class="page-scroll" href="#location">Location</a></li>
+				</ul>
+			</nav>
+			<section id="homepage" class="area col-md-12">
+				<h1>Vleminck X</h1>
+				<h3>SausMeesters</h1>
+			</section>
+		</header>
+		<main>
+			<section id="hours">
+				<section id="hoursleft">
+					<h3>Hours Open</h3>
+					<p></p>
+				</section>
+			</section>
+			<section id="menu"></section>
+			<section id="about"></section>
+			<section id="location"></section>
+		</main>
+		<footer>
+
+		</footer>
+	</body>
 </html>
