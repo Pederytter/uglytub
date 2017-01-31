4ebbba8e1081ad671b4b1442e26fe25f3b05f799
Pederytter <pederytter@gmail.com>
2017-01-31 09:24:29 +0100

    Struktur første udgave

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<<nav>
+			
+		</nav>
+	</header>
+	<main>
+		
+	</main>
+	<footer>
+		
+	</footer>
+</body>
+</html>
