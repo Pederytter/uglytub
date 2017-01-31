939b3a9c0e8a23bc3b348a239216cc48ad4b2ed2
Pederytter <pederytter@gmail.com>
2017-01-31 10:09:34 +0100

    Første udgave af header og nav

--- index.php
+++ index.php
@@ -3,16 +3,26 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/scrolling-nav.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<header>
-		<<nav>
-			
+		<nav class="col-md-12">
+			<ul>
+			<a href="#homepage"><img id="logo" src="images/logo.png" alt="Vleminck Logo"></a>
+				<li><a class="page-scroll" href="#homepage">Homepage</a></li>
+				<li><a class="page-scroll" href="#menu">Menu</a></li>
+				<li><a class="page-scroll" href="#about">About US</a></li>
+				<li><a class="page-scroll" href="#location">Location</a></li>
+			</ul>
 		</nav>
 	</header>
 	<main>
-		
+		<section id="homepage" class="area"></section>
+		<section id="menu"></section>
+		<section id="about"></section>
+		<section id="location"></section>
 	</main>
 	<footer>
 		
